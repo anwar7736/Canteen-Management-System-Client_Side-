c1e8fecd8e4966d7b7efe1d42ad680d921caf5a2
anwar7736 <anwarhossain7736@gmail.com>
2022-01-08 22:10:32 +0600

    Canteen Management System

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import cogoToast from 'cogo-toast';
+function App() {
+  return (
+    <div className="App">
+      <h1 className="alert alert-success" style={{fontWeight: 'bolder', padding : '10px'}}>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
